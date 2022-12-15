f4c76fd10d3ad6ab542c1e9234d6951d6f384e6a
RodrigoLee56 <sousarodrigo56@outlook.com>
2022-12-15 15:12:07 -0400

    Projeção dos dados do autor com DTO

--- src/main/java/com/hawk/workshopmongo/domain/Post.java
+++ src/main/java/com/hawk/workshopmongo/domain/Post.java
@@ -6,21 +6,23 @@ import java.util.Objects;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.hawk.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
-
-	private User author;
+	private AuthorDTO author;
 
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -61,11 +63,11 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
--- src/main/java/com/hawk/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/hawk/workshopmongo/dto/AuthorDTO.java
@@ -4,21 +4,18 @@ import java.io.Serializable;
 
 import com.hawk.workshopmongo.domain.User;
 
-public class UserDTO implements Serializable {
-	
+public class AuthorDTO implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
 	private String id;
 	private String name;
-	private String email;
 
-	public UserDTO() {
+	public AuthorDTO() {
 	}
 
-	public UserDTO(User user) {
-		id = user.getId();
-		name = user.getName();
-		email = user.getEmail();
+	public AuthorDTO(User author) {
+		id = author.getId();
+		name = author.getName();
 	}
 
 	public String getId() {
@@ -35,14 +32,6 @@ public class UserDTO implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 }
