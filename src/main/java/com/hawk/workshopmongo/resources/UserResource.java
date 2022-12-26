c6b1528b12d634658e0654b2cc799e9aae5c6082
RodrigoLee56 <sousarodrigo56@outlook.com>
2022-12-26 18:29:48 -0400

    Obtendo um post por id

--- src/main/java/com/hawk/workshopmongo/resources/UserResource.java
+++ src/main/java/com/hawk/workshopmongo/resources/UserResource.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
