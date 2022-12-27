4bc6317c15a2fa19354279c12b2afdacd9affb00
RodrigoLee56 <sousarodrigo56@outlook.com>
2022-12-27 02:14:47 -0400

    Consulta simples com query methods

--- src/main/java/com/hawk/workshopmongo/resources/PostResource.java
+++ src/main/java/com/hawk/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.hawk.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hawk.workshopmongo.domain.Post;
+import com.hawk.workshopmongo.resources.util.URL;
 import com.hawk.workshopmongo.services.PostService;
 
 @RestController
@@ -21,5 +25,19 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = postService.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@RequestMapping(value = "/titlesearchic", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitleIgnoreCase(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = postService.findByTitleIgnoreCase(text);
+		return ResponseEntity.ok().body(posts);
 	}
 }
