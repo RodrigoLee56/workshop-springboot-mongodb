797bc50e5934274717fedaae0c00715841f62cb9
RodrigoLee56 <sousarodrigo56@outlook.com>
2022-12-28 01:22:09 -0400

    Consulta com vários critérios

--- src/main/java/com/hawk/workshopmongo/resources/PostResource.java
+++ src/main/java/com/hawk/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.hawk.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,24 +21,37 @@ public class PostResource {
 
 	@Autowired
 	private PostService postService;
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
 	}
-	
+
 	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> posts = postService.findByTitle(text);
 		return ResponseEntity.ok().body(posts);
 	}
-	
+
 	@RequestMapping(value = "/titlesearchic", method = RequestMethod.GET)
-	public ResponseEntity<List<Post>> findByTitleIgnoreCase(@RequestParam(value = "text", defaultValue = "") String text) {
+	public ResponseEntity<List<Post>> findByTitleIgnoreCase(
+			@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> posts = postService.findByTitleIgnoreCase(text);
 		return ResponseEntity.ok().body(posts);
 	}
+
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> posts = postService.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(posts);
+	}
+
 }
